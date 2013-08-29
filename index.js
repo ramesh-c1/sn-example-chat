047543859ac611c08e6390a6bf5d66372228fcbf
Sam Roberts <vieuxtech@gmail.com>
2013-08-29 14:04:42 -0700

    Optional clustering of chat and urlsaver sample

--- index.js
+++
@@ -1,8 +0,0 @@
-if (require.main === module) {
-  require('./bin/simple');
-} else {
-  module.exports = {
-    SimpleServer: require('./lib/simple'),
-    WorkerServer: require('./lib/worker')
-  };
-}
